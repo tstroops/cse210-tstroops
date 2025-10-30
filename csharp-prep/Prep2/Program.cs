7150ef2d97db158c0c2b5db190c1fe6c5fea9b04
tstroops <tomstroops@gmail.com>
2025-10-30 15:53:50 -0600

    completed Prep 1 and Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letterGrade = "";
+        Console.Write("Enter your grade (0-100): ");
+        grade = Convert.ToInt16(Console.ReadLine());
+        if (grade >= 90)
+        {
+            letterGrade += 'A';
+        }
+        else if (grade >= 80)
+        {
+            letterGrade += 'B';
+        }
+        else if (grade >= 70)
+        {
+            letterGrade += 'C';
+        }
+        else if (grade >= 60)
+        {
+            letterGrade += 'D';
+        }
+        else
+        {
+            letterGrade += 'F';
+        }
+        if (letterGrade != "F")
+        {
+            if (grade % 10 >= 7 && letterGrade != "A")
+            {
+                letterGrade += '+';
+            }
+            else if (grade%10 < 3)
+            {
+                letterGrade += '-';
+            }
+        }
+        Console.WriteLine(letterGrade);
     }
 }
